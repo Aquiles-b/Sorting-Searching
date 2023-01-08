15da27650fa41151b314ee60cb9ada56b546827b
Aquiles-bb <brunoaquiles@ufpr.br>
2023-01-08 19:29:20 -0300

    Selection sort feito e main.c mais organizado.

--- main.c
+++ main.c
@@ -4,67 +4,90 @@
 
 #include "ordenacao.h"
 
+void imprime_vet(int *vet, int tam){
+    for(int i=0; i < tam; i++){
+        printf("%d ", vet[i]);
+    }
+    printf ("\n");
+}
+
 int main(){
-	char nome[MAX_CHAR_NOME];
-	int idxBusca;
-	int numComp;
+    char nome[MAX_CHAR_NOME];
+    int idxBusca;
+    int numComp;
 
-	//Dica: somente é posśivel criar vetores grandes utilizando alocação dinâmica de memória
-	//Veja um exemplo de alocação dinâmica a seguir
-	int tamVetor = 3;
-	int* vetor = malloc(tamVetor * sizeof(int));
-	if(vetor == NULL){
-		printf("Falha fatal. Impossível alocar memoria.");
-		return 1;
-	}
-	//Depois de alocado, o vetor pode ser utilizado normalmente
-	//Não esqueça de desalocar no final do programa via free
-		
-	vetor[0] = 1;
-	vetor[1] = 10;
-	vetor[2] = 12;
+    //Dica: somente é posśivel criar vetores grandes utilizando alocação dinâmica de memória
+    //Veja um exemplo de alocação dinâmica a seguir
+    int tamVetor = 10;
+    int* vetor = malloc(tamVetor * sizeof(int));
+    if(vetor == NULL){
+        printf("Falha fatal. Impossível alocar memoria.");
+        return 1;
+    }
+    //Depois de alocado, o vetor pode ser utilizado normalmente
+    //Não esqueça de desalocar no final do programa via free
 
-	getNome(nome);
-	printf("Trabalho de %s\n", nome);
-	printf("GRR %u\n", getGRR());
-	printf("GRR 20221225\n");
+    /*Vetor que o professor passou tem tamanho 3
+    * com os elemento 1, 10 e 12 nessa ordem.*/
+    vetor[0] = 21;
+    vetor[1] = 0;
+    vetor[2] = 12;
+    vetor[3] = 3;
+    vetor[4] = 1;
+    vetor[5] = 5;
+    vetor[6] = 6;
+    vetor[7] = 4;
+    vetor[8] = 22;
+    vetor[9] = 8;
 
-	//Para medir o tempo, inclua time.h, e siga o exemplo:
-	clock_t start, end;//variáveis do tipo clock_t
+    getNome(nome);
+    printf("Trabalho de %s\n", nome);
+    printf("GRR %u\n", getGRR());
+    printf("GRR 20221225\n");
+
+    //Para medir o tempo, inclua time.h, e siga o exemplo:
+    clock_t start, end;//variáveis do tipo clock_t
     double total;
 
+    imprime_vet(vetor, tamVetor);
+
+    /*Selection sort.*/
+    start = clock();
+    numComp = selectionSort(vetor, 10);
+    end = clock();
+    printf("Selection Sort Custo: %d\n", numComp);
+    total = ((double)end - start)/CLOCKS_PER_SEC;
+    printf("Tempo total: %f\n", total);
+
+    imprime_vet(vetor, tamVetor);
+
+    /* Tem que implementar esses ainda. */
+    /* numComp = insertionSort(vetor, 3); */
+    /* numComp = mergeSort(vetor, 3); */
+    /* numComp = quickSort(vetor, 3); */
+    /* numComp = heapSort(vetor, 3); */
+
+    /*Busca sequencial*/
     numComp = 0;
-	start = clock();//start recebe o "ciclo" corrente
-	idxBusca = buscaSequencial(vetor, 3, 10, &numComp);
-	end = clock();//end recebe o "ciclo" corrente
-	printf("Busca sequencial %d %d\n", idxBusca, numComp);
-	//o tempo total é a diferença dividia pelos ciclos por segundo
-	total = ((double)end - start)/CLOCKS_PER_SEC;
-	printf("Tempo total: %f\n", total);
-    
+    start = clock();//start recebe o "ciclo" corrente
+    idxBusca = buscaSequencial(vetor, 10, 10, &numComp);
+    end = clock();//end recebe o "ciclo" corrente
+    printf("Busca sequencial indice: %d Custo: %d\n", idxBusca, numComp);
+    //o tempo total é a diferença dividia pelos ciclos por segundo
+    total = ((double)end - start)/CLOCKS_PER_SEC;
+    printf("Tempo total: %f\n", total);
+
+    /*Busca binaria */
     numComp = 0;
-	start = clock();
-	idxBusca = buscaBinaria(vetor, 3, 12, &numComp);
-	end = clock();
-	printf("Busca binaria %d %d\n", idxBusca, numComp);
-	total = ((double)end - start)/CLOCKS_PER_SEC;
-	printf("Tempo total: %f\n", total);
+    start = clock();
+    idxBusca = buscaBinaria(vetor, 10, 12, &numComp);
+    end = clock();
+    printf("Busca binaria indice: %d Custo: %d\n", idxBusca, numComp);
+    total = ((double)end - start)/CLOCKS_PER_SEC;
+    printf("Tempo total: %f\n", total);
 
+    //É obrigatório que você libere a memória alocada dinamicamente via free
+    free(vetor);
 
-	/* numComp = insertionSort(vetor, 3); */
-	/* numComp = selectionSort(vetor, 3); */
-	/* numComp = mergeSort(vetor, 3); */
-	/* numComp = quickSort(vetor, 3); */
-	/* numComp = heapSort(vetor, 3); */
-
-	for(int i=0; i < 3; i++){
-		printf("%d ", vetor[i]);
-	}
-
-    printf("\n");
-
-	//É obrigatório que você libere a memória alocada dinamicamente via free
-	free(vetor);
-
-	return 0;
+    return 0;
 }
--- ordenacao.c
+++ ordenacao.c
@@ -1,16 +1,15 @@
 #include "ordenacao.h"
-#include <stdio.h>
 #include <string.h>
 
 void getNome(char nome[]){
-	//substitua por seu nome
-	strncpy(nome, "Bruno Aquiles de Lima e Marcelo Gyovani Pereira", MAX_CHAR_NOME);
-	nome[MAX_CHAR_NOME-1] = '\0';//adicionada terminação manual para caso de overflow
+    //substitua por seu nome
+    strncpy(nome, "Bruno Aquiles de Lima e Marcelo Gyovani Pereira", MAX_CHAR_NOME);
+    nome[MAX_CHAR_NOME-1] = '\0';//adicionada terminação manual para caso de overflow
 }
 
 //a função a seguir deve retornar o seu número de GRR
 unsigned int getGRR(){
-	return 20221230;
+    return 20221230;
 }
 
 int buscaSequencial(int vetor[], int tam, int valor, int* numComparacoes){
@@ -23,10 +22,10 @@ int buscaSequencial(int vetor[], int tam, int valor, int* numComparacoes){
     }
     (*numComparacoes)++;
 
-	return buscaSequencial(vetor, tam-1, valor, numComparacoes);
+    return buscaSequencial(vetor, tam-1, valor, numComparacoes);
 }
 
-int b_Binaria(int vetor[], int a, int b, int valor, int *numComparacoes){
+int aux_busca_binaria(int vetor[], int a, int b, int valor, int *numComparacoes){
     if (a >= b)
         return -1;
 
@@ -37,39 +36,62 @@ int b_Binaria(int vetor[], int a, int b, int valor, int *numComparacoes){
     }
     if (valor > vetor[m] ){
         (*numComparacoes) += 2;
-        return b_Binaria(vetor, m+1, b, valor, numComparacoes);
+        return aux_busca_binaria(vetor, m+1, b, valor, numComparacoes);
     }
 
     (*numComparacoes) += 2;
-    return b_Binaria(vetor, a, m, valor, numComparacoes);
+    return aux_busca_binaria(vetor, a, m, valor, numComparacoes);
 }
 
 int buscaBinaria(int vetor[], int tam, int valor, int* numComparacoes){
 
-    return b_Binaria(vetor, 0, tam, valor, numComparacoes);
+    return aux_busca_binaria(vetor, 0, tam, valor, numComparacoes);
 }
 
 int insertionSort(int vetor[], int tam){	
-	vetor[0] = 99;
-	return -1;
+    vetor[0] = 99;
+    return -1;
+}
+
+int aux_selection(int vetor[], int a, int b, int *comparacoes){
+    int menor;
+    int i, aux;
+
+    if (a == b)
+        return 1;
+
+    menor = a;
+    for (i = a+1; i <= b; i++){
+        (*comparacoes)++;
+        if (vetor[menor] > vetor[i])
+            menor = i;
+    }
+    aux = vetor[menor];
+    vetor[menor] = vetor[a];
+    vetor[a] = aux;
+
+    return aux_selection(vetor, a+1, b, comparacoes);
 }
 
 int selectionSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+    int comparacoes = 0;
+
+    aux_selection(vetor, 0, tam-1, &comparacoes);
+
+    return comparacoes;
 }
 
 int mergeSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+    vetor[0] = 99;
+    return -1;
 }
 
 int quickSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+    vetor[0] = 99;
+    return -1;
 }
 
 int heapSort(int vetor[], int tam){
-	vetor[0] = 99;
-	return -1;
+    vetor[0] = 99;
+    return -1;
 }
