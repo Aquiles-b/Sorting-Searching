90dd29917a1cfdd693e3899587a98689f6d6f6b2
marzelop <maagyo.teo23@gmail.com>
2023-01-25 21:33:54 -0300

    Insertion, Heap e Quicksort feitos
    Provavelmente tem algum bug, mas é isso ae

--- ordenacao.c
+++ ordenacao.c
@@ -46,13 +46,50 @@ int aux_busca_binaria(int vetor[], int a, int b, int valor, int *numComparacoes)
 }
 
 int buscaBinaria(int vetor[], int tam, int valor, int* numComparacoes){
-
     return aux_busca_binaria(vetor, 0, tam, valor, numComparacoes);
 }
 
+void troca(int *a, int *b) {
+    int aux = *a;
+
+    *a = *b;
+    *b = aux;
+}
+
+/* Retorna a posição de inserção de um elemento x num vetor de inicio a e fim b */
+int buscaBinariaInsercao(int x, int vetor[], int a, int b, int *numC) {
+    if (a > b)
+        return b + 1;
+    int meio = (a + b) / 2;
+    if (vetor[meio] > x) {
+        *numC++;
+        return buscaBinariaInsercao(x, vetor, a, meio - 1, numC);
+    }
+    if (vetor[meio] < x) {
+        *numC++;        
+        return buscaBinariaInsercao(x, vetor, meio + 1, b, numC);
+    }
+    return meio;
+}
+
+void insere(int vetor[], int a, int b, int *numC) {
+    int p = buscaBinariaInsercao(vetor[b], vetor, a, b, numC);
+    for (int i = b; i > p + 1; i--)
+        troca(vetor + i, vetor + i - 1);
+}
+
+void auxInsertionSort(int vetor[], int a, int b, int *numC) {
+    if (a >= b)
+        return;
+    auxInsertionSort(vetor, a, b - 1, numC);
+    insere(vetor, a, b, numC);
+}
+
 int insertionSort(int vetor[], int tam){	
-    vetor[0] = 99;
-    return -1;
+    int numC = 0;
+
+    auxInsertionSort(vetor, 0, tam - 1, &numC);
+    return numC;
 }
 
 int aux_selection(int vetor[], int a, int b, int *comparacoes){
@@ -127,13 +164,82 @@ int mergeSort(int vetor[], int tam){
     aux_merge(vetor, 0, tam-1, &numComparacoes);
     return numComparacoes;
 }
-/* teste */
+
+
+
+int particiona(int vetor[], int a, int b, int x, int *numC) {
+    int meio = a - 1;
+    for (int i = a; i < b; i++) {
+        if (vetor[i] <= x) {
+            meio++;
+            troca(vetor + meio, vetor + i);
+        }
+        *numC++;
+    }
+
+    return meio;
+}
+
+void auxQuickSort(int vetor[], int a, int b, int *numC) {
+    if (a >= b)
+        return;
+    int meio = particiona(vetor, a, b, vetor[b], numC);
+    auxQuickSort(vetor, a, meio - 1, numC);
+    auxQuickSort(vetor, meio + 1, b, numC);
+}
+
 int quickSort(int vetor[], int tam){
-    vetor[0] = 99;
-    return -1;
+    int numC = 0;
+
+    auxQuickSort(vetor, 0, tam - 1, &numC);
+    return numC;
+}
+
+/* Retorna o indíce do filho esquerdo de um nodo de uma árvore binária */
+int filhoE(int i) {
+    return 2*i + 2;
+}
+
+/* Retorna o indíce do filho direito de um nodo de uma árvore binária */
+int filhoD(int i) {
+    return 2*i + 3;
+}
+
+void maxHeapify(int vetor[], int i, int tam, int *numC) {
+    int esq = filhoE(i);
+    int dir = filhoD(i);
+    int maior;
+
+    if (esq < tam) {
+        if (vetor[esq] > vetor[i])
+            maior = esq;
+        *numC++;
+    }
+    else
+        maior = i;
+    if (dir < tam) {
+        if (vetor[dir] > vetor[maior])
+            maior = dir;
+        *numC++;
+    }
+    if (maior != i) {
+        troca(vetor + i, vetor + maior);
+        maxHeapify(vetor, maior, tam, numC);
+    }
+}
+
+void buildMaxHeap(int vetor[], int tam, int *numC) {
+    for (int i = tam / 2; i > 0; i--)
+        maxHeapify(vetor, i, tam, numC);
 }
 
 int heapSort(int vetor[], int tam){
-    vetor[0] = 99;
-    return -1;
+    int numC = 0;
+
+    buildMaxHeap(vetor, tam, &numC);
+    for (int i = tam; i > 1; i--) {
+        troca(vetor, vetor + i);
+        maxHeapify(vetor, 1, i, &numC);
+    }
+    return numC;
 }
